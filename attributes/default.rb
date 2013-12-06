2833067e647de4d3bfd4539153a1a291694f1c81
Ryutaro YOSHIBA <ryuzee@gmail.com>
2013-12-07 08:14:45 +0900

    add lang

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,5 +4,6 @@ default["trac"]["svn_repository_root_dir"] = "/opt/svn"
 default["trac"]["password_file"] = "/opt/trac_svn_password"
 default["trac"]["admin_account"] = "admin"
 default["trac"]["admin_password"] = "ADMIN"
+default["trac"]["lang"] = "en_US.UTF-8"
 
 # vim: filetype=ruby.chef
--- recipes/default.rb
+++ recipes/default.rb
@@ -94,7 +94,7 @@ when "centos", "redhat", "amazon", "scientific", "fedora"
   end
 
   ## create trac project
-  e = execute "trac-admin #{node["trac"]["trac_root_dir"]}/#{node["trac"]["project_name"]} initenv #{node["trac"]["project_name"]} sqlite:db/trac.db svn #{node["trac"]["svn_repository_root_dir"]}/#{node["trac"]["project_name"]}" do
+  e = execute "LANG=#{node["trac"]["lang"]} trac-admin #{node["trac"]["trac_root_dir"]}/#{node["trac"]["project_name"]} initenv #{node["trac"]["project_name"]} sqlite:db/trac.db svn #{node["trac"]["svn_repository_root_dir"]}/#{node["trac"]["project_name"]}" do
     action :run
     not_if do File.exists?("#{node["trac"]["trac_root_dir"]}/#{node["trac"]["project_name"]}/conf/trac.ini") end
   end
